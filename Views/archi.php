0888978e45bbe86ca4d78d405bdbe76508602c2a
Ricardo <ricardo.remond.pro@gmail.com>
2024-03-07 12:32:56 +0100

    Css page Arch en cours

--- Views/archi.php
+++ Views/archi.php
@@ -19,6 +19,24 @@
         <h3>Nos Projets Architecture</h3>
     </div>
 
+    <div class="faitInteressant">
+        <div class="row">
+            <h3 class="col s3">Quelques faits interessants</h3>
+            <div class="col s3">
+                <h3>147</h3>
+                <h5>Projets</h5>
+            </div>
+            <div class="col s3">
+                <h3>117</h3>
+                <h5>Clients satisfaits</h5>
+            </div>
+            <div class="col s3">
+                <h3>7</h3>
+                <h5>Années d'experiences</h5>
+            </div>
+
+        </div>
+    </div>
 
     <?php
     include "../Templates/footer-desktop.php"
