b05423d2b9168edb0ef6dc690029526fe3034f03
Ricardo <ricardo.remond.pro@gmail.com>
2024-03-09 17:59:04 +0100

    Page Archi redac projet en cours / img fullscreen

--- Views/archi.php
+++ Views/archi.php
@@ -37,72 +37,93 @@
 
         </div>
     </div>
-    <div class="container">
-        <div>
-            <div class="titre">
-                <p class="valign-wrapper">[2023 - 2024]</p>
-                <h4>
-                    Odysée - Urbain
-                </h4>
+
+
+    <ul class="gallery">
+        <li>
+            <div class="container">
+                <div>
+                    <div class="titre">
+                        <p class="valign-wrapper">[2023 - 2024]</p>
+                        <h4>
+                            Odysée - Urbain
+                        </h4>
+                    </div>
+
+                    <div class="right-align">
+                        <p>
+                            Aménagement d’un
+                            parc résidentiel avec
+                            la construction de
+                            logements collectifs.
+                        </p>
+                    </div>
+
+                </div>
             </div>
 
-            <div class="right-align">
-                <p>
-                    Aménagement d’un
-                    parc résidentiel avec
-                    la construction de
-                    logements collectifs.
+            <div class="valign-wrapper">BRON [69]
+                Analyse urbaine
+            </div>
+
+            <div class="row">
+                <p class="col s12 m8 l8">
+                    ¹ Ce projet vise à élaborer un plan directeur pour la ville de Bron, proposant
+                    un nouvel aménagement urbain attentif aux besoins des citadins. Dans
+                    cette nouvelle ère, l’humanité est confrontée à de multiples menaces
+                    qui pourraient potentiellement entraîner l’extinction de toutes les
+                    formes de vie sur la planète Gaia, à long terme (bien que cette notion
+                    soit relative par rapport à notre existence dans l’échelle temporelle).
+
+                    <br>
+                    <br>
+
+
+                    ² Il devient impératif de prendre conscience et de comprendre le fonctionnement
+                    de notre zone critique afin de proposer des solutions durables pour le
+                    développement. Dans ce cas d’étude spécifique, les menaces identifiées
+                    soulignent des opportunités qu’il est essentiel de saisir pour atteindre les
+                    objectifs énoncés. Il sera nécessaire de proposer des idées novatrices
+                    pour la conception de la ville, en cherchant à l’améliorer par une réflexion
+                    approfondie. La centralité repose sur la diversité et l’abondance des individus
+                    qui composent la société, ainsi que sur les fonctions qui façonnent l’économie.
                 </p>
+
+                <p class="col s12 m4 l4">
+                    ³ Les lieux de centralité, superposés ou juxtaposés aux dimensions de
+                    nœud et de pôle, contribuent à la création du phénomène urbain. Dans
+                    ce schéma, les lieux de centralité sont intrinsèquement liés aux pôles,
+                    qui attirent et concentrent les flux de personnes et de biens, ainsi qu’aux
+                    nœuds (comme l’avenue Salengro), qui facilitent l’accès aux lieux de
+                    centralité et de polarité.et de biens, ainsi qu’aux nœuds (comme l’avenue
+                    Salengro), qui facilitent l’accès aux lieux de centralité et de polarité.
+                </p>
+
+            </div>
+            <div class="row">
+                <span class="col s6">
+                    <p>ISOMÉTRIE TOPOGRAPHIQUE - Carte de stratégie </p>
+                    <p>Illustrator-Auto CAD </p>
+                </span>
+
+                <p class="col s6 right-align">Piéce graphique: Guerin Kelliane / Tom Soule-Beaud</p>
             </div>
 
-        </div>
-    </div>
-    <div class="valign-wrapper">BRON [69]
-        Analyse urbaine</div>
-    <div class="row">
-        <p class="col s8">
-            ¹ Ce projet vise à élaborer un plan directeur pour la ville de Bron, proposant
-            un nouvel aménagement urbain attentif aux besoins des citadins. Dans
-            cette nouvelle ère, l’humanité est confrontée à de multiples menaces
-            qui pourraient potentiellement entraîner l’extinction de toutes les
-            formes de vie sur la planète Gaia, à long terme (bien que cette notion
-            soit relative par rapport à notre existence dans l’échelle temporelle).
-
-            <br>
-            <br>
+            <img id="image" class="responsive-img" src="../assets/projet-archi/isometrie1.png" alt="Carte de stratégie">
+        </li>
+        <li style="background: #f7ede2;"></li>
+        <li style="background: #f5cac3;"></li>
+        <li style="background: #84a59d;"></li>
+        <li style="background: #f28482;"></li>
+    </ul>
 
 
-            ² Il devient impératif de prendre conscience et de comprendre le fonctionnement
-            de notre zone critique afin de proposer des solutions durables pour le
-            développement. Dans ce cas d’étude spécifique, les menaces identifiées
-            soulignent des opportunités qu’il est essentiel de saisir pour atteindre les
-            objectifs énoncés. Il sera nécessaire de proposer des idées novatrices
-            pour la conception de la ville, en cherchant à l’améliorer par une réflexion
-            approfondie. La centralité repose sur la diversité et l’abondance des individus
-            qui composent la société, ainsi que sur les fonctions qui façonnent l’économie.
-        </p>
-
-        <p class="col s4">
-            ³ Les lieux de centralité, superposés ou juxtaposés aux dimensions de
-            nœud et de pôle, contribuent à la création du phénomène urbain. Dans
-            ce schéma, les lieux de centralité sont intrinsèquement liés aux pôles,
-            qui attirent et concentrent les flux de personnes et de biens, ainsi qu’aux
-            nœuds (comme l’avenue Salengro), qui facilitent l’accès aux lieux de
-            centralité et de polarité.et de biens, ainsi qu’aux nœuds (comme l’avenue
-            Salengro), qui facilitent l’accès aux lieux de centralité et de polarité.
-        </p>
-
-        <img class="responsive-img" src="../assets/projet-archi/isometrie.png" alt="">
-    </div>
-
-
-
-    </div>
     <!-- <img class="responsive-img" src="../assets/projet-archi/iconesCarte.png" alt="">
             <img class="responsive-img" src="../assets/projet-archi/CarteSynthesefinal.png" alt=""> -->
     <?php
     include "../Templates/footer-desktop.php"
         ?>
+    <script src="../assets/js/script.js"></script>
 </body>
 
 </html>
---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+const slider = document.querySelector('.gallery');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', e => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', _ => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', _ => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const SCROLL_SPEED = 3;
+    const walk = (x - startX) * SCROLL_SPEED;
+    slider.scrollLeft = scrollLeft - walk;
+});
+
+// Récupérez l'élément image par son ID
+let elem = document.getElementById("image");
+
+// Ajoutez un gestionnaire d'événements pour le clic sur l'image
+elem.addEventListener('click', () => {
+    if (document.fullscreenElement) {
+        // Si l'image est déjà en mode plein écran, désactivez-le
+        document.exitFullscreen();
+    } else {
+        // Sinon, activez le mode plein écran pour l'image
+        elem.requestFullscreen();
+    }
+});
