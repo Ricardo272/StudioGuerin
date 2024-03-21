80a4985eeeacdb76025b5e429a00c8f3cfb5bd27
Ricardo <ricardo.remond.pro@gmail.com>
2024-03-21 21:45:53 +0100

    page projet en cours

--- Views/odysee-urbain.php
+++ Views/odysee-urbain.php
@@ -32,13 +32,63 @@
                     alt='Image de présentation du projet'>
                 <ul class="collapsible popout">
                     <li>
-                        <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-                        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                        <div class="collapsible-header"><i class="material-icons">location_city</i></div>
+                        <div class="collapsible-body"><span>BRON [69]
+                                Analyse urbaine</span></div>
                     </li>
                 </ul>
             </div>
             <div class="col s12 m6 l4">
                 <img class="responsive-img" src="../assets/projet-archi/isometrie1.png" alt="Carte de stratégie">
+                <ul class="collapsible popout">
+                    <li>
+                        <div class="collapsible-header"><i class="material-icons">filter_1</i></div>
+                        <div class="collapsible-body">
+                            <span>
+                                ¹ Ce projet vise à élaborer un plan directeur pour la ville
+                                de Bron, proposant
+                                un nouvel aménagement urbain attentif aux besoins des citadins. Dans
+                                cette nouvelle ère, l’humanité est confrontée à de multiples menaces
+                                qui pourraient potentiellement entraîner l’extinction de toutes les
+                                formes de vie sur la planète Gaia, à long terme (bien que cette notion
+                                soit relative par rapport à notre existence dans l’échelle temporelle).
+                            </span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="collapsible-header"><i class="material-icons">filter_2</i></div>
+                        <div class="collapsible-body">
+                            <span>
+                                ² Il devient impératif de prendre conscience et de
+                                comprendre le fonctionnement
+                                de notre zone critique afin de proposer des solutions durables pour le
+                                développement. Dans ce cas d’étude spécifique, les menaces identifiées
+                                soulignent des opportunités qu’il est essentiel de saisir pour atteindre les
+                                objectifs énoncés. Il sera nécessaire de proposer des idées novatrices
+                                pour la conception de la ville, en cherchant à l’améliorer par une réflexion
+                                approfondie. La centralité repose sur la diversité et l’abondance des individus
+                                qui composent la société, ainsi que sur les fonctions qui façonnent l’économie.
+                            </span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="collapsible-header"><i class="material-icons">filter_3</i></div>
+                        <div class="collapsible-body">
+                            <span>
+                                ³ Les lieux de centralité, superposés ou juxtaposés aux dimensions de
+                                nœud et de pôle, contribuent à la création du phénomène urbain. Dans
+                                ce schéma, les lieux de centralité sont intrinsèquement liés aux pôles,
+                                qui attirent et concentrent les flux de personnes et de biens, ainsi qu’aux
+                                nœuds (comme l’avenue Salengro), qui facilitent l’accès aux lieux de
+                                centralité et de polarité.et de biens, ainsi qu’aux nœuds (comme l’avenue
+                                Salengro), qui facilitent l’accès aux lieux de centralité et de polarité.
+                            </span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col s12 m12 l4">
+                <img class="responsive-img" src="../assets/projet-archi/iso-colo.jpg" alt='Image iso colo'>
                 <ul class="collapsible popout">
                     <li>
                         <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
@@ -47,7 +97,17 @@
                 </ul>
             </div>
 
-            <div class="col s12 m12 l4">
+            <div class="col s12 m6 l6">
+                <img class="responsive-img" src="../assets/projet-archi/plan-masse.jpg" alt='Image plan masse'>
+                <ul class="collapsible popout">
+                    <li>
+                        <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
+                        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col s12 m6 l6">
                 <img class="responsive-img" src="../assets/projet-archi/plan-masse-commun.png"
                     alt='Image plan masse commun'>
                 <ul class="collapsible popout">
@@ -58,27 +118,7 @@
                 </ul>
             </div>
 
-            <div class="col s12 m6 l4">
-                <img class="responsive-img" src="../assets/projet-archi/plan-masse.jpg" alt='Image plan masse'>
-                <ul class="collapsible popout">
-                    <li>
-                        <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-                        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="col s12 m6 l4">
-                <img class="responsive-img" src="../assets/projet-archi/iso-colo.jpg" alt='Image iso colo'>
-                <ul class="collapsible popout">
-                    <li>
-                        <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-                        <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="col s12 m12 l8">
+            <div class="col s12 m12 l12">
                 <img class="responsive-img" src="../assets/projet-archi/Facadepsdnord.jpg"
                     alt='Image de présentation du projet'>
                 <ul class="collapsible popout">
